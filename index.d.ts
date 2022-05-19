99380af6c20c49f29535775a47eb14773c77cba5
Martin Heidegger <martin.heidegger@gmail.com>
2022-05-19 23:43:34 +0900

    feat: adding support for ISearchResult by the dynamoDB

--- index.d.ts
+++ index.d.ts
@@ -35,7 +35,7 @@ export interface Model {
 }
 // TODO: this is a stub for the DB interface of @tradle/dynamodb
 export interface DB {
-  find: (opts: any) => Promise<any/* SearchResult */>
+  find: (opts: any) => Promise<ISearchResult>
 }
 export interface Bot {
   models: {
@@ -283,3 +283,40 @@ export interface Utils {
   }>
 }
 
+export type IDynamoDBKey = {
+  hashKey: string
+  rangeKey?: string
+}
+
+export interface DynamoDBProjection {
+  /**
+   * The set of attributes that are projected into the index:    KEYS_ONLY - Only the index and primary keys are projected into the index.    INCLUDE - In addition to the attributes described in KEYS_ONLY, the secondary index will include other non-key attributes that you specify.    ALL - All of the table attributes are projected into the index.  
+   */
+  ProjectionType?: ProjectionType;
+  /**
+   * Represents the non-key attribute names which will be projected into the index. For local secondary indexes, the total count of NonKeyAttributes summed across all of the local secondary indexes, must not exceed 20. If you project the same attribute into two different indexes, this counts as two distinct attributes when determining the total.
+   */
+  NonKeyAttributes?: string[];
+}
+export type ProjectionExpression = string;
+export type ProjectionType = "ALL"|"KEYS_ONLY"|"INCLUDE"|string;
+
+export interface IDynogelIndex extends IDynamoDBKey {
+  name: string
+  type: 'global' | 'local'
+  projection: DynamoDBProjection
+}
+
+interface IItemPosition {
+  [key: string]: any
+}
+
+export type ItemToPosition = (item: any) => IItemPosition
+
+export interface ISearchResult {
+  items: any[]
+  itemToPosition: ItemToPosition
+  startPosition: IItemPosition
+  endPosition: IItemPosition
+  index: IDynogelIndex
+}
